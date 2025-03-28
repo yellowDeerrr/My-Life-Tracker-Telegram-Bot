a01933c89a04362925a9a19a6ad708615ad28207
yellowDeerrr <emailforgithubbanksystemsql@gmail.com>
2025-03-28 22:47:11 +0200

    i dont know, trying to fix

--- Database.py
+++ Database.py
@@ -1,70 +1,73 @@
 import psycopg2
 import configparser
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+def connect():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
 
-db_host = config.get("database", "host")
-db_port = config.getint("database", "port")  # Convert to integer
-db_name = config.get("database", "database")
-db_user = config.get("database", "user")
-db_password = config.get("database", "password")
+    db_host = config.get("database", "host")
+    db_port = config.get("database", "port")  # Convert to integer
+    db_name = config.get("database", "database")
+    db_user = config.get("database", "user")
+    db_password = config.get("database", "password")
 
-try:
-    # Connect to PostgreSQL
-    connection = psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        database=db_name,
-        user=db_user,
-        password=db_password
-    )
-
-    # Create a cursor object
-    cursor = connection.cursor()
-
-    cursor.execute("""CREATE TABLE params (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            Health INT,
-            Strength INT,
-            Intelligence INT,
-            Charisma INT,
-            Self_Discipline INT,
-            Confidence INT,
-            Happiness INT,
-            Recovery INT,
-            Skills INT,
-            Wisdom INT
+    try:
+        # Connect to PostgreSQL
+        connection = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_password
         )
-        """)
-    connection.commit()
-    cursor.execute("""INSERT INTO params (
-            Health,
-            Strength,
-            Intelligence,
-            Charisma,
-            Self_Discipline,
-            Confidence,
-            Happiness,
-            Recovery,
-            Skills,
-            Wisdom
-        ) VALUE (10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
-        """)
-    connection.commit()
-    cursor.execute("""CREATE TABLE history (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        description TEXT,
-        update_param VARCHAR
-    );
-        """)
-    connection.commit()
-except Exception as e:
-    print(f"Error: {e}")
 
-finally:
-    # Close the connection
-    if 'connection' in locals() and connection:
-        cursor.close()
-        connection.close()
-        print("Database connection closed.")
+        # Create a cursor object
+        cursor = connection.cursor()
+
+        cursor.execute("""CREATE TABLE params (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                Health INT,
+                Strength INT,
+                Intelligence INT,
+                Charisma INT,
+                Self_Discipline INT,
+                Confidence INT,
+                Happiness INT,
+                Recovery INT,
+                Skills INT,
+                Wisdom INT
+            )
+            """)
+        connection.commit()
+        cursor.execute("""INSERT INTO params (
+                Health,
+                Strength,
+                Intelligence,
+                Charisma,
+                Self_Discipline,
+                Confidence,
+                Happiness,
+                Recovery,
+                Skills,
+                Wisdom
+            ) VALUE (10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
+            """)
+        connection.commit()
+        cursor.execute("""CREATE TABLE history (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            description TEXT,
+            update_param VARCHAR
+        );
+            """)
+        connection.commit()
+        return cursor
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Close the connection
+        if 'connection' in locals() and connection:
+            cursor.close()
+            connection.close()
+            print("Database connection closed.")
+
--- actions.py
+++ actions.py
@@ -1,12 +1,14 @@
-from Database import cursor
-
+from Database import *
 
 def selectAllFromHistory():
+    cursor = connect()
     cursor.execute("SELECT * FROM history")
     return cursor.fetchall()
 
 def selectAllFromParams():
+    cursor = connect()
     cursor.execute("SELECT * FROM params")
     return cursor.fetchall()
 def addHealth():
+    cursor = connect()
     cursor.execute("INSERT INTO params (health) VALUES ()")
